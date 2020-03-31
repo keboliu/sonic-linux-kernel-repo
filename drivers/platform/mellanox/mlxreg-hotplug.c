f428b2f8dfae14573ace89739ebb740e479cddb2
Vadim Pasternak <vadimp@mellanox.com>
2020-04-01 01:55:55 +0300

    From 32d24501f00b494b011d34be7c751a168940dff3 Mon Sep 17 00:00:00 2001 Subject: [platform-next v1] platform/mellanox: mlxreg-io: Add support for  complex attributes
    Add support for attributes composed from few registers.
    Such attributes could occupy from 2 to 4 sequential registers.
    This is not the case for double word size register space, for word size
    register space complex attribute can occupy up to two register, for
    byte size - up to four. These attributes can carry, for example, CPLD
    or FPGA versioning, power consuming info, etcetera.
    
    Signed-off-by: Vadim Pasternak <vadimp@mellanox.com>

--- drivers/platform/mellanox/mlxreg-hotplug.c
+++ drivers/platform/mellanox/mlxreg-hotplug.c
@@ -204,9 +204,26 @@ static int mlxreg_hotplug_attr_init(struct mlxreg_hotplug_priv_data *priv)
 
 	/* Go over all kinds of items - psu, pwr, fan. */
 	for (i = 0; i < pdata->counter; i++, item++) {
+		if (item->capability) {
+			/*
+			 * Read group capability register to get actual number
+			 * of interrupt capable components and set group mask
+			 * accordingly.
+			 */
+			ret = regmap_read(priv->regmap, item->capability,
+					  &regval);
+			if (ret)
+				return ret;
+
+			item->mask = GENMASK((regval & item->mask) - 1, 0);
+		}
+
 		data = item->data;
 		/* Go over all units within the item. */
 		for (j = 0, k = 0; j < item->count; j++, data++) {
+			/* Skip if bit in mask is not set. */
+			if (!(item->mask & BIT(j)))
+				continue;
 			if (data->capability) {
 				/*
 				 * Read capability register and skip non
@@ -519,20 +536,6 @@ static int mlxreg_hotplug_set_irq(struct mlxreg_hotplug_priv_data *priv)
 	item = pdata->items;
 
 	for (i = 0; i < pdata->counter; i++, item++) {
-		if (item->capability) {
-			/*
-			 * Read group capability register to get actual number
-			 * of interrupt capable components and set group mask
-			 * accordingly.
-			 */
-			ret = regmap_read(priv->regmap, item->capability,
-					  &regval);
-			if (ret)
-				goto out;
-
-			item->mask = GENMASK((regval & item->mask) - 1, 0);
-		}
-
 		/* Clear group presense event. */
 		ret = regmap_write(priv->regmap, item->reg +
 				   MLXREG_HOTPLUG_EVENT_OFF, 0);
@@ -671,11 +674,8 @@ static int mlxreg_hotplug_probe(struct platform_device *pdev)
 		priv->irq = pdata->irq;
 	} else {
 		priv->irq = platform_get_irq(pdev, 0);
-		if (priv->irq < 0) {
-			dev_err(&pdev->dev, "Failed to get platform irq: %d\n",
-				priv->irq);
+		if (priv->irq < 0)
 			return priv->irq;
-		}
 	}
 
 	priv->regmap = pdata->regmap;
