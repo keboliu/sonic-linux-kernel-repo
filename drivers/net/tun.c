da9093b9af454a6bf4a1cb516df50558825cf71f
admin <admin@debian>
2022-11-01 06:21:03 +0000

    From 7f7d6a7dd958ff68da96a78442ffa6a404aa976a Mon Sep 17 00:00:00 2001
    Subject: [PATCH] TUNTAP driver support carrier configuration

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -2293,6 +2293,18 @@ static long __tun_chr_ioctl(struct file *file, unsigned int cmd,
 		ret = 0;
 		break;
 
+    case TUNSETCARRIER:
+        if (arg) {
+            netif_carrier_on(tun->dev);
+		} else {
+            netif_carrier_off(tun->dev);
+        }
+
+		tun_debug(KERN_INFO, tun, "carrier %s\n",
+			  arg ? "on" : "off");
+
+		break;
+
 	default:
 		ret = -EINVAL;
 		break;
